6fc48064ca6e27a2e6adfa1752d1be0e63bd436e
GrafAleksander <aleks.tarnovskyi@gmail.com>
2018-07-26 20:50:44 +0300

    Кривой бек-енд

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
+mongoose.connect("mongodb://localhost/schools");
 app.set("view engine" , "ejs");
 
+//Схема для школ 
+var schoolsShema = new mongoose.Schema({
+  name : String,
+  image : String,
+  site : String 
+});
+
+var schools = mongoose.model("schools" , schoolsShema);
+
+schools.create({
+  name : "№329 'Логос'",
+  image : "http://logos.kiev.ua/pix/main.jpg",
+  site : "http://logos.kiev.ua/"
+}) , 
+function(err , schools){
+  if(err){
+    console.log("Вот такую фигню ты сделал , кожаный мешок");
+    console.log(err);
+  }
+  else{
+    console.log("И что ты здесь выучишь ? ");
+    consoele.log(schools);
+  }
+}
 app.get('/', function (req, res) {
   res.render('index');
   console.log("Кто-то наехал на мою главную страницу , кожаный ублюдок!")
+  schools.find({} , function(err , schools){
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.render('index' , {schools : schools});
+    }
+  });
 });
 
 app.get('/info', function (req, res) {
@@ -26,3 +60,11 @@ app.get('/reg' , function(req, res){
 app.listen(3000, function () {
   console.log('Сервер пашит , а ты нет , кожаный мешок !');
 });
+
+/*
+Пользаватеь в фильтрах указывает значание
+Значиние передаётся в виде переменных 
+Переменные передаются в db.find
+Дальше из db.find мы достаём нужные намданные 
+Делаем вывод в виде карточек 
+*/
